dc05e7c8d46472729c899b6c5162ffd5d8ad1cdc
Nur Arief Fauzian Rakhman <79679064+XnomaX@users.noreply.github.com>
2025-06-15 13:31:03 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -38,12 +38,11 @@ y = df['survived']
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Training dan logging
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Akurasi: {acc}")
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model", input_example=X_test.iloc[:5])
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print(f"Akurasi: {acc}")
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.log_model(model, "model", input_example=X_test.iloc[:5])
