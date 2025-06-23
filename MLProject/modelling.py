3a5535ed85bce3ffbd225dc896f288eac78171cd
XnomaX <nurarief1123@gmail.com>
2025-06-24 03:20:53 +0700

    Changes to be committed: 	new file:   MLProject/Artifact_Gdrive.txt 	new file:   MLProject/DockerHub_Link.txt 	modified:   MLProject/modelling.py
    git push origin main

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,8 +1,6 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-import joblib
-import shutil
 import os
 import argparse
 from sklearn.model_selection import train_test_split
@@ -35,7 +33,7 @@ df = pd.read_csv(data_file)
 X = df.drop(columns=["survived"])
 y = df["survived"]
 
-# Ambil 1000 data untuk training
+# Ambil 1000 data untuk training agar model ringan untuk komputasi rendah
 X_train, _, y_train, _ = train_test_split(
     X, y, train_size=1000, stratify=y, random_state=42
 )
@@ -43,7 +41,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X_train, y_train, test_size=0.2, random_state=42
 )
 
-# Model ringan
+# Model 
 model = RandomForestClassifier(
     n_estimators=100,
     max_depth=10,
@@ -54,6 +52,3 @@ model = RandomForestClassifier(
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
-mlflow.log_metric("accuracy", acc)
-# Log model ke MLflow (dan simpan manual juga)
-mlflow.sklearn.log_model(model, "model", input_example=X_test.iloc[:5])
